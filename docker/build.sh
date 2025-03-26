0aed8d055598c361d54564cd3e2f168fa456becb
Vsevolod Hulchuk <vsevolod.hulchuk@gmail.com>
2025-03-26 14:06:04 +0100

    change to sev's docker images

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 docker compose -f docker-compose.yaml -f docker-compose-dev.yaml build
-export DOCKER_REGISTRY="ghcr.io/comrob"
+export DOCKER_REGISTRY="ghcr.io/sevahul"
 export DOCKER_IMAGE="slam-bench"
 export DOCKER_TAG="latest"
 docker tag $DOCKER_IMAGE:$DOCKER_TAG $DOCKER_REGISTRY/$DOCKER_IMAGE:$DOCKER_TAG
